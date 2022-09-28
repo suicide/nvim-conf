6ffc1a9c67287f7a7624dae1c6db62ec788cffed
Patrick Sy <patrick.sy@get-it.us>
2022-09-29 00:48:11 +0200

    move config to use autoload

--- init.lua
+++ init.lua
@@ -1,17 +1,1 @@
 require('plugins')
-require('common')
-require('theme')
-require('common-init')
-require('keymaps')
-
-require('conf-telescope')
-require('conf-gitsigns')
-
-require('conf-treesitter')
-require('conf-treesitter-context')
-
-require('conf-cmp')
-
-require('conf-lsp')
-
-require('conf-comment')
