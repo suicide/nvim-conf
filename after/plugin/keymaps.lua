005c0851aa57052eebef0ac10cc5e0ec1f02b5b9
Patrick Sy <patrick.sy@get-it.us>
2023-03-06 00:39:06 +0100

    cut and delete keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -4,3 +4,15 @@ vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '{{', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', '}}', vim.diagnostic.goto_next, opts)
 -- vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist, opts)
+
+-- delete
+vim.keymap.set('n', 'x', '"_x', opts)
+vim.keymap.set('n', 'd', '"_d', opts)
+vim.keymap.set('v', 'd', '"_d', opts)
+vim.keymap.set('n', 'D', '"_D', opts)
+
+-- cut
+vim.keymap.set('n', '<Leader>x', '"+d', opts)
+vim.keymap.set('v', '<Leader>x', '"+d', opts)
+vim.keymap.set('n', '<Leader>xx', '"+dd', opts)
+vim.keymap.set('n', '<Leader>X', '"+D', opts)
