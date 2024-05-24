b1eda92dcb41dea1f0696a2d6048e3ffb941444c
suiiii <suiiii@suiiii.rip>
2024-05-24 18:47:29 +0200

    feat: add lazy updates to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,23 +2,33 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'kyazdani42/nvim-web-devicons' },
-    opts = {
-      sections = {
-        lualine_c = {
-          {
-            'filename',
-            path = 1,
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_x = {
+            {
+              require("lazy.status").updates,
+              cond = require("lazy.status").has_updates,
+              color = { fg = "#ff9e64" },
+            },
+            'encoding', 'fileformat', 'filetype'
+          },
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
+        },
+        inactive_sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
           }
-        }
-      },
-      inactive_sections = {
-        lualine_c = {
-          {
-            'filename',
-            path = 1,
-          }
-        }
-      }
-    },
+        },
+      })
+    end
   }
 }
