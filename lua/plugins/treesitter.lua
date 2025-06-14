4231af6d69cafa9546c619644de46f1be5ff1eee
suiiii <suiiii@suiiii.rip>
2025-06-14 19:33:14 +0200

    fix(treesitter): Removed ipkg as repo is missing

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
         require('nvim-treesitter.configs').setup {
           ensure_installed = "all",
           sync_install = false,
+          ignore_install = {"ipkg"},
 
           highlight = {
             enable = true,
